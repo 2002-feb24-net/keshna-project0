edf9f6e85332facbe3200d54f3f3abc0be594b6d
kesh24 <keshnarana4@gmail.com>
2020-03-21 14:10:22 -0500

    database successfully connected

---
+++ CupCakeData/Model/Location.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CupCakeData.Model
+{
+    public partial class Location
+    {
+        public Location()
+        {
+            Orders = new HashSet<Orders>();
+            Store = new HashSet<Store>();
+        }
+
+        public int Id { get; set; }
+        public string Zipcode { get; set; }
+
+        public virtual ICollection<Orders> Orders { get; set; }
+        public virtual ICollection<Store> Store { get; set; }
+    }
+}
---
+++ CupCakeData/Model/Orders.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CupCakeData.Model
+{
+    public partial class Orders
+    {
+        public Orders()
+        {
+            OrderProduct = new HashSet<OrderProduct>();
+        }
+
+        public int Id { get; set; }
+        public int LocationId { get; set; }
+        public int CustomerId { get; set; }
+        public DateTime OrderTime { get; set; }
+
+        public virtual Customer Customer { get; set; }
+        public virtual Location Location { get; set; }
+        public virtual ICollection<OrderProduct> OrderProduct { get; set; }
+    }
+}
