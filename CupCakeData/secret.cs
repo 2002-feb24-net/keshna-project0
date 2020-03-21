74a714ca052a78683c8bd6ac806ab8e026606891
kesh24 <keshnarana4@gmail.com>
2020-03-21 14:45:34 -0500

    git

--- CupCakeData/secret.cs
+++ CupCakeData/secret.cs
@@ -7,7 +7,7 @@ namespace CupCakeData
     internal class secret
     {
 
-        internal const string ConnectionString = "Server=tcp:keshna.database.windows.net,1433;Initial Catalog=CupCakeShop;Persist Security Info=False;User ID=keshna;Password=Kesh123!N;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;"
+        internal const string ConnectionString = "Server=tcp:keshna.database.windows.net,1433;Initial Catalog=CupCakeShop;Persist Security Info=False;User ID=keshna;Password=Nimesh1!K;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;"
     }
 
 }
