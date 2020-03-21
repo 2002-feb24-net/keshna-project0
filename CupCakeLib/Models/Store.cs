85676cb1911a152c6528564b5b3d94b67d7d2ef0
kesh24 <keshnarana4@gmail.com>
2020-03-20 22:31:51 -0500

    step1 done

---
+++ CupCakeLib/Models/Store.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+
+namespace CupCake
+{
+    class Store
+    {
+        //the locations
+        public int StoreId { get; set; }
+        public string StoreName { get; set; }
+        public string ZipCode { get; set; }
+
+    }
+}
